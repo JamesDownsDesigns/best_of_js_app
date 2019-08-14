ff8ba6b1c4964b71560faee8706ebb790cde047d
Joseph Fitzpatrick <josephfitzpatrick333@yahoo.com>
2019-08-14 09:31:34 -0500

    Added components to fetch data from Github API, and pass that data to child component

--- src/components/parentToChild/child.js
+++ src/components/parentToChild/child.js
@@ -7,7 +7,19 @@ const child = props => (
   <div>
     <ul>
       {props.json.map(jsonItem => (
-        <li>{jsonItem.name}</li>
+        <div>
+          <h1>Search Result: {jsonItem.name}</h1>
+          <ul>
+            <li>Tags: {jsonItem.tags_url}</li>
+            <li>Link to Repository: {jsonItem.html_url}</li>
+            <li>Last Updated At: {jsonItem.updated_at}</li>
+            <li>License: {jsonItem.license.name}</li>
+            <li>Stars: {jsonItem.stargazers_count}</li>
+            <li>Image: Have not figured it out yet lol :(</li>
+          </ul>
+          
+        </div>
+        
       ))}
     </ul>
   </div>
