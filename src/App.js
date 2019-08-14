bd94017b3c9436fbb25360cfbd17bfa3cfc26203
cindeezo <49541255+cindeezo@users.noreply.github.com>
2019-08-13 21:49:48 -0400

    add animate.css

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Hello from './components/Hello';
 import DetectChrome from './components/DetectChrome';
 import './styles/main.scss';
+import { Animated } from 'react-animated-css';
 
 const App = () => (
   <div>
